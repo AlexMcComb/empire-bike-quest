e0b29b0ebe8465819723b465c3fd40c7ea05b891
Alex <alex@Alexs-MacBook-Pro.local>
2018-12-13 15:44:43 -0700

    added mock data and app interface

---
+++ public/app.js
@@ -0,0 +1,77 @@
+// this is mock data, but when I create my API
+// I'll have it return data that looks like this
+var MOCK_JOB_REQUESTS = {
+	"jobRequests": [
+        {
+            "id": "1111111",
+            "Company": "Green Hub",
+            "companyId": "aaaaaa",
+            "Description": "30 letters weighing 1lb",
+            "pickupLocation":"75 9th Ave, New York, NY 10011",
+            "dropoffLocation":"131 W 3rd St, New York, NY 10012",
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "2222222",
+            "Company": "Blue Hub",
+            "companyId": "bbbbbb",
+            "Description": "1 box 3x3 in. 2lbs",
+            "pickupLocation":"228 Park Ave S, New York, NY 10003",
+            "dropoffLocation":"126 2nd Ave, New York, NY 10003",
+            "publishedAt": 1470012976609
+        },
+        {
+            "id": "333333",
+            "Company": "Orange Hub",
+            "companyId": "oooooo",
+            "Description": "1 box 5x3 in. 4lbs",
+            "pickupLocation":"151 W 46th St, New York, NY 10036",
+            "dropoffLocation":"205 E 99th St, New York, NY 10029",
+            "publishedAt": 1470011976609
+        },
+        {
+            "id": "4444444",
+            "Company": "Purple Hub",
+            "companyId": "ppppppp",
+            "Description": "2 boxex 2x3 in. 6lbs",
+            "pickupLocation":"1000 5th Ave, New York, NY 10028",
+            "dropoffLocation":"Second Ave. bet. E. 59 St. to, E 60th St, New York, NY 10022",
+            "publishedAt": 1470009976609
+        }
+    ]
+};
+
+// this function's name and argument can stay the
+// same after we have a live API, but its internal
+// implementation will change. Instead of using a
+// timeout function that returns mock data, it will
+// use jQuery's AJAX functionality to make a call
+// to the server and then run the callbackFn
+function getJobRequests(callbackFn) {
+    // we use a `setTimeout` to make this asynchronous
+    // as it would be with a real AJAX call.
+	setTimeout(function(){ callbackFn(MOCK_JOB_REQUESTS)}, 1);
+}
+
+// this function stays the same when we connect
+// to real API later
+function displayJobRequests(data) {
+    for (index in data.jobRequests) {
+	   $('body').append(
+        '<p>' + data.jobRequests[index].Company + '</p>',
+        '<p>' + data.jobRequests[index].Description + '</p>',
+        '<p>' + data.jobRequests[index].pickupLocation + '</p>',
+        '<p>' + data.jobRequests[index].dropoffLocation + '</p>');
+    }
+}
+
+// this function can stay the same even when we
+// are connecting to real API
+function getAndDisplayJobRequests() {
+	getJobRequests(displayJobRequests);
+}
+
+//  on page load do this
+$(function() {
+	getAndDisplayJobRequests();
+})
